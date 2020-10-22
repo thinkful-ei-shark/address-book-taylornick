80ace90a2be03d253fa314a0f0d2602ffaf44eb9
nickjang <nickjang@alumni.upenn.edu>
2020-10-22 11:42:08 -0400

    Add /POST address

--- src/addressBook.js
+++ src/addressBook.js
@@ -12,26 +12,29 @@
 
 const { v4: uuid } = require('uuid');
 
-module.exports = [
-  {
-    id: uuid(),
-    firstName: 'Taylor',
-    lastName: 'Phelps',
-    address1: '1234 Address Street',
-    address2: 'Apt 520',
-    city: 'Place',
-    state: 'WA',
-    zip: 54891,
-  },
+module.exports = {
+  addresses: [
+    {
+      id: uuid(),
+      firstName: 'Taylor',
+      lastName: 'Phelps',
+      address1: '1234 Address Street',
+      address2: 'Apt 520',
+      city: 'Place',
+      state: 'WA',
+      zip: 54891,
+    },
 
-  {
-    id: uuid(),
-    firstName: 'Nick',
-    lastName: 'Jang',
-    address1: '4321 Place Street',
-    address2: 'Apt 790',
-    city: 'Address',
-    state: 'PA',
-    zip: 67891,
-  },
-];
+    {
+      id: uuid(),
+      firstName: 'Nick',
+      lastName: 'Jang',
+      address1: '4321 Place Street',
+      address2: 'Apt 790',
+      city: 'Address',
+      state: 'PA',
+      zip: 67891,
+    },
+  ],
+  uuid
+};
--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const addresses = require('./addressBook.js');
+const { PORT, NODE_ENV } = require('./config');
+const { addresses, uuid } = require('./addressBook.js');
+const addressBook = require('./addressBook.js');
 
 console.log(addresses);
 
 const app = express();
+app.use(express.json());
 
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
@@ -17,7 +19,58 @@ app.use(helmet());
 app.use(cors());
 
 app.get('/address', (req, res) => {
-  res.send(addresses);
+  res
+    .json(addresses);
+});
+
+app.post('/address', (req, res) => {
+  const { firstName, lastName, address1,
+    address2 = '', city, state } = req.body;
+  let { zip } = req.body;
+
+  if (!firstName)
+    res.status(400).json({ error: 'Please give a first name.' });
+
+  if (!lastName)
+    res.status(400).json({ error: 'Please give a last name.' });
+
+  if (!address1)
+    res.status(400).json({ error: 'Please give a street address.' });
+
+  if (!city)
+    res.status(400).json({ error: 'Please give a city.' });
+
+  if (!state)
+    res.status(400).json({ error: 'Please give a state.' });
+
+  if (!zip)
+    res.status(400).json({ error: 'Please give a zip code.' });
+
+  if (state.length !== 2)
+    res.status(400).json({ error: 'Please enter a state that is two characters.' });
+
+  if (zip.trim().length !== 5)
+    res.status(400).json({ error: 'Please enter a zipcode that is five numbers.' });
+
+  zip = parseInt(zip);
+  const id = uuid();
+  const address = {
+    id,
+    firstName,
+    lastName,
+    address1,
+    address2,
+    city,
+    state,
+    zip
+  };
+
+  addressBook.push(address);
+
+  res
+    .status(201)
+    .location( `http://localhost:${PORT}/address/${id}`)
+    .json(address);
 });
 
 
